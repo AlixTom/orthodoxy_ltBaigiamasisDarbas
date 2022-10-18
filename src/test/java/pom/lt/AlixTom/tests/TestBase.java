ba4a07cea5d5c4a7c9b5dfe16149bd5c1f1a66ed
Alinute <aiatvideo@yandex.ru>
2022-10-18 14:20:46 +0300

    Before and After methods added

---
+++ src/test/java/pom/lt/AlixTom/tests/TestBase.java
@@ -0,0 +1,23 @@
+package pom.lt.AlixTom.tests;
+
+import pom.lt.AlixTom.pages.Common;
+import lt.AlixTom.utils.TestListener;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import pom.lt.AlixTom.utils.TestListener;
+
+@Listeners(TestListener.class)
+public class TestBase {
+
+    @BeforeMethod
+    public void setUp() {
+        Common.setUpDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        Common.closeDriver();
+    }
+
+}
